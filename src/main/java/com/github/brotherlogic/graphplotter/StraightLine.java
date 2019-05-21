bf7b1627e581d09c630d75b7584f4d58d3b4297a
brotherlogic <brotherlogic@gmail.com>
2019-05-21 12:38:31 -0700

    Fixer

--- src/main/java/com/github/brotherlogic/graphplotter/StraightLine.java
+++ src/main/java/com/github/brotherlogic/graphplotter/StraightLine.java
@@ -3,7 +3,7 @@ package com.github.brotherlogic.graphplotter;
 public class StraightLine extends Line {
 
     long m;
-    long c
+    long c;
 
     public StraightLine(long start, long end, int startV, int endV) {
         m =(end-start)  / (endV - startV);
