a32a4e1cc082673e79cbef3b11482cd3172167dd
brotherlogic <brotherlogic@gmail.com>
2019-05-21 14:06:32 -0700

    Fixed line spec

--- src/main/java/com/github/brotherlogic/graphplotter/GraphPanel.java
+++ src/main/java/com/github/brotherlogic/graphplotter/GraphPanel.java
@@ -28,12 +28,13 @@ public class GraphPanel extends JPanel {
            long startTime = LocalDateTime.parse("2019-01-01T00:00:00").toEpochSecond(ZoneOffset.UTC);
            long endTime = LocalDateTime.parse("2020-01-01T00:00:00").toEpochSecond(ZoneOffset.UTC);
 
-           int lastPointY = 0;
+           int lastPointY = h-10;
            for(int x = 1; x < w-15; x++){
                long timeP = ((endTime-startTime)/(w-15))*x+startTime;
                int value = l.getValue(timeP);
                g.drawLine(x-1+5,h-10-lastPointY, x+5, h-10-value);
                lastPointY = h-10-value;
+               System.out.println("Point = " + lastPointY);
            }
         }
     }
