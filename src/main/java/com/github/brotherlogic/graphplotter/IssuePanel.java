38b490c14ac297950bfd618ac464d4fd49b0df01
brotherlogic <brotherlogic@gmail.com>
2019-07-03 15:41:24 -0700

    Added basic issue panel

---
+++ src/main/java/com/github/brotherlogic/graphplotter/IssuePanel.java
@@ -0,0 +1,14 @@
+package com.github.brotherlogic.graphplotter;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class IssuePanel extends JPanel {
+    String issue = "Current issue";
+    JLabel label = new JLabel(issue);
+
+    public IssuePanel() {
+        this.add(label);
+    }
+}
