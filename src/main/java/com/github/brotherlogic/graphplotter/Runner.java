073657f9216f12763cf0577b2f0f28a5812ba558
Simon Tucker <simon@brotherlogic@gmail>
2019-09-12 10:43:14 -0700

    Refresh display every minute

--- src/main/java/com/github/brotherlogic/graphplotter/Runner.java
+++ src/main/java/com/github/brotherlogic/graphplotter/Runner.java
@@ -92,7 +92,7 @@ public class Runner extends JavaServer {
                 }
 
                 try {
-                    Thread.sleep(5 * 1000);
+                    Thread.sleep(60 * 1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
