7d950f82ece4a5277167107ffa1277b4e7689697
Simon Tucker <simon@brotherlogic@gmail>
2020-02-10 17:46:40 -0800

    Skips home tasks on dekstop display

--- src/main/java/com/github/brotherlogic/graphplotter/Getter.java
+++ src/main/java/com/github/brotherlogic/graphplotter/Getter.java
@@ -36,18 +36,17 @@ public class Getter {
     public String getLatestIssue() throws Exception {
         String response = "No Response";
         if (host != null) {
+	    ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();;
 	    try{
-            ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();
-
-            GithubGrpc.GithubBlockingStub client = GithubGrpc.newBlockingStub(channel);
-	    Issue issue =  client.withDeadlineAfter(10, TimeUnit.SECONDS).getAll(Githubcard.GetAllRequest.newBuilder().setLatestOnly(true).build()).getIssues(0);
-	    if (issue.getDateAdded() < System.currentTimeMillis()/1000 - 60*60*2) {
-		response = issue.getTitle() + " - " + issue.getUrl();
-	    } else {
-		response = "All done currently";
-	    }
+		GithubGrpc.GithubBlockingStub client = GithubGrpc.newBlockingStub(channel);
+		Issue issue =  client.withDeadlineAfter(10, TimeUnit.SECONDS).getAll(Githubcard.GetAllRequest.newBuilder().setLatestOnly(true).addAvoid("home").build()).getIssues(0);
+		if (issue.getDateAdded() < System.currentTimeMillis()/1000 - 60*60*2) {
+		    response = issue.getTitle() + " - " + issue.getUrl();
+		} else {
+		    response = "All done currently";
+		}
 	    } finally {
-            channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
+		channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
 	    }
         }
         return response;
